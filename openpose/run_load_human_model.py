6f72e674bf20b8b216e0cb39c2ff90170b9ac319
kenkenhed <kenk1543@gmail.com>
2019-12-23 16:18:46 +0900

    kusocode

--- openpose/run_load_human_model.py
+++ openpose/run_load_human_model.py
@@ -1,5 +1,4 @@
 import pickle
-from tf_pose import common
 import cv2
 
 image_w = 1024
@@ -8,12 +7,36 @@ f = open("humans_model.txt","rb")
 humans = pickle.load(f)
 f.close()
 
+# class CocoPart(Enum):
+#     Nose = 0
+#     Neck = 1
+#     RShoulder = 2
+#     RElbow = 3
+#     RWrist = 4
+#     LShoulder = 5
+#     LElbow = 6
+#     LWrist = 7
+#     RHip = 8
+#     RKnee = 9
+#     RAnkle = 10
+#     LHip = 11
+#     LKnee = 12
+#     LAnkle = 13
+#     REye = 14
+#     LEye = 15
+#     REar = 16
+#     LEar = 17
+#     Background = 18
+
+
+common_values = 18
+
 for i,human in enumerate(humans):
     print(i,': times')
     print('x: ',human)
     
     # draw point
-    for i in range(common.CocoPart.Background.value):
+    for i in range(common_values):
         
         if i not in human.body_parts.keys():
             continue
