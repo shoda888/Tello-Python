42a8a34d558769e765b5688f0608aa037aa21e44
kyathrien <uraru0719@gmail.com>
2019-12-26 01:51:33 +0900

    判別に必要な関節データの抽出とベクトルの角度を計算する関数を追加

--- openpose/run_load_human_model.py
+++ openpose/run_load_human_model.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 def read_dict(file):
     with open(file, newline = "") as f:
@@ -8,11 +9,27 @@ def read_dict(file):
 
         for row in read_dict:
             for k, v in row.items():
-                return_dict[k].append(v) # notice the type of the value is always string.   
+                return_dict[k].append(v) # notice the type of the value is always string.
 
     return return_dict
 
 
 data = read_dict("uncho.csv")
-print(data)
-print(data["1"])
+#1:首，2:右肩，3:右肘，5:左肩，6:左肘，8:右尻，11:左尻
+neaaa = [float(s) for s in data["1"]];
+rsho = [float(v) for v in data["2"]];
+relb = [float(v) for v in data["3"]];
+lsho = [float(v) for v in data["5"]];
+lelb = [float(v) for v in data["6"]];
+rhip = [float(v) for v in data["8"]];
+lhip = [float(v) for v in data["9"]];
+
+#ベクトルの角度を計算
+def vec_angle(v1,v2):
+    v1length = math.sqrt(pow(v1[0],2)+pow(v1[1],2));
+    v2length = math.sqrt(pow(v2[0],2)+pow(v2[1],2));
+    in_pro = v1[0]*v2[0] + v1[1]*v2[1];
+    cos = in_pro/(v1length*v2length);
+    theta = math.degrees(math.acos(cos));
+
+    return theta
