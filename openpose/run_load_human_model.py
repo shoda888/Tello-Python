f55a6fd6e2c7d77414076b83d869f02ef88e96a3
kyathrien <uraru0719@gmail.com>
2019-12-31 17:14:39 +0900

    label.csvに姿勢のラベルを随時記載する機能を追加

--- openpose/run_load_human_model.py
+++ openpose/run_load_human_model.py
@@ -1,5 +1,6 @@
 import csv
 import math
+import numpy as np
 
 def read_dict(file):
     with open(file, newline = "") as f:
@@ -16,13 +17,27 @@ def read_dict(file):
 
 data = read_dict("uncho.csv")
 #1:首，2:右肩，3:右肘，5:左肩，6:左肘，8:右尻，11:左尻
-neaaa = [float(s) for s in data["1"]];
+neck = [float(s) for s in data["1"]];
+neck = np.array(neck[0:2]);
 rsho = [float(v) for v in data["2"]];
+rsho = np.array(rsho[0:2]);
 relb = [float(v) for v in data["3"]];
+relb = np.array(relb[0:2]);
 lsho = [float(v) for v in data["5"]];
+lsho = np.array(lsho[0:2]);
 lelb = [float(v) for v in data["6"]];
+lelb = np.array(lelb[0:2]);
 rhip = [float(v) for v in data["8"]];
-lhip = [float(v) for v in data["9"]];
+rhip = np.array(rhip[0:2]);
+lhip = [float(v) for v in data["11"]];
+lhip = np.array(lhip[0:2]);
+
+#ベクトルの設定
+#p1:首から右尻，p2:首から左尻，p3:右肩から右肘，p4:左肩から左肘
+p1 = neck - rhip;
+p2 = neck - lhip;
+p3 = rsho - relb;
+p4 = lsho - lelb;
 
 #ベクトルの角度を計算
 def vec_angle(v1,v2):
@@ -33,3 +48,22 @@ def vec_angle(v1,v2):
     theta = math.degrees(math.acos(cos));
 
     return theta
+
+#右腕，左腕の角度
+rang = vec_angle(p1,p3);
+lang = vec_angle(p2,p4);
+
+if rang >= 90 and lang < 90:
+    #右手のみ挙げている場合(label=0)
+    label = 0;
+elif rang < 90 and lang >= 90:
+    #左手のみ挙げている場合(label=1)
+    label = 1;
+else:
+    #右手も左手も挙げている場合(label=2)
+    label = 2;
+
+
+with open('label.csv', 'a', newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow([label])
