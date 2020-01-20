ab804baa8a8e777abb9ad9b442f21d7e06990af2
TakamiMizuki <58679874+TakamiMizuki@users.noreply.github.com>
2020-01-20 23:54:06 +0900

    アプローチ
    修正とcv-faceからの動作を加えた

--- SystemPJ/rec_human/approachModule.py
+++ SystemPJ/rec_human/approachModule.py
@@ -3,6 +3,7 @@ import cv2
 from detect_video import img, width, height
 from tello import Tello
 import time
+from main.py import x, y, w, h
 import matplotlib.pyplot as plt
 #import cv2 mainであるから必要なし?
 
@@ -29,7 +30,7 @@ class Approach:
                     ・TLD
                     ・MedianFlow　これが最適?
                 の4種類から選択(詳細はググれ)
-    """
+
     #drone = Tello('', 8889, command_timeout=.01)
     #操作するドローン
     #画像とbbox追加
@@ -43,7 +44,7 @@ class Approach:
     cv::Rect2d roiMEDIANFLOW = roi
 
     #cv2.bbox()
-
+"""
     def __init__(self, drone, frame, bbox, track_type):
         """
         Approachクラスの初期化
@@ -53,21 +54,34 @@ class Approach:
         self.tracker.init(frame, bbox) # 作成したトラッカーの初期化．画像と認識した人の領域を与える
 
     def select_tracker(self, track_type):
+        choice = input("Please select your tracker number: ")
+
+        if choice == '0':
+            tracker = cv2.TrackerBoosting_create()
+        if choice == '1':
+            tracker = cv2.TrackerMIL_create()
+        if choice == '2':
+            tracker = cv2.TrackerKCF_create()
+        if choice == '3':
+            tracker = cv2.TrackerTLD_create()
+        if choice == '4':
+            tracker = cv2.TrackerMedianFlow_create()
+
         """
         コンストラクタで用いるトラッカー初期化用のメソッド
-        """
+
         Args:
             track_type str:
                 指定されたタイプのトラッカー
 
         Returns:
             tracker obj:
-                指定されたタイプのトラッカーインスタンス
+               指定されたタイプのトラッカーインスタンス
         """
-        tracker = cv2.TrackerBoosting_create() # テスト用に入れた
+        #tracker = cv2.TrackerBoosting_create() # テスト用に入れた
 
         return tracker
-        """
+
 
     def approach(self, frame):
         """
@@ -81,45 +95,86 @@ class Approach:
         # 追跡が成功し，接近できた場合にはdrone.close_flagを立てる
         """
 
-        success, roi = tracker.update(frame)
+        success, roi = self.tracker.update(frame)
         (x,y,w,h) = tuple(map(int,roi))
-        if sucess:
+        self.drone.approach_flag　== 0#見つけてないため
+        if sucess:#追跡状態
         """
         searchメソッドで得たbboxを元に，cv2のトラッキングを用いながら
         被災者に接近するメソッド
         追跡できているとき
+        制御部分の管理
         """
         #bboxの中心を判定　4辺の位置を平均化
         #頂点の位置(22,134)(257,44)...から作られる長方形
         #bbox_length 縦横
-            bcent = (0.5*(bbox[0]+bbox[2]),(0.5*(bbox[1]+bbox[3])))
-            fcent = (240,180)#画像の中心点 ここは詳しく書き込み必須
+        """ 試しに自作したとこ
+        bcent = (int(0.5*(bbox[0]+bbox[2])),int(0.5*(bbox[1]+bbox[3])))
+        fcent = (240,180)#画像の中心点 ここは詳しく書き込み必須
 
-            #追跡部分
-            #距離判定は人のbox比/全体サイズで行う
-            if (detect_video.width)*(detect_video.height)/(172800) < 0.1:#人との距離を保つ bbox小さくなりすぎた時
-                drone.move_forward(0.2)
-            elif (detect_video.width)*(detect_video.height)/(172800) > 0.6:
-                drone.move_backward(0.2)
-            else:
-                drone.move_stop#ホバリングのまま
+        #追跡部分
+        #距離判定は人のbox比/全体サイズで行う
+        if (detect_video.width)*(detect_video.height)/(172800) < 0.1:#人との距離を保つ bbox小さくなりすぎた時
+            drone.move_forward(0.2)
+        elif (detect_video.width)*(detect_video.height)/(172800) > 0.6:
+            drone.move_backward(0.2)
+        else:
+            drone.move_stop#ホバリングのまま
 
-            if (bcent(1,1)-fcent(1,1)) > 80:#見つけた人が中央から外れたら中央に移動する操作
-                drone.move_left(0.05)
-                drone.rotate_ccw(5)
-            elif (bcent(1,1)-fcent(1,1)) < -80:#優先度は上から順に?
-                drone.move_right(0.05)
-                drone.rotate_cw(5)
-            elif (bcent(2,2)-fcent(2,2)) > 40:
-                drone.move_up(0.05)
-            elif (bcent(2,2)-fcent(2,2)) < -40:
-                drone.move_down(0.05)
-            else:
-                drone.move_stop#ホバリングのまま　このプログラムは対話時も維持
+        if (bcent(1,1)-fcent(1,1)) > 80:#見つけた人が中央から外れたら中央に移動する操作
+            drone.move_left(0.05)
+            drone.rotate_ccw(5)
+        elif (bcent(1,1)-fcent(1,1)) < -80:#優先度は上から順に?
+            drone.move_right(0.05)
+            drone.rotate_cw(5)
+        elif (bcent(2,2)-fcent(2,2)) > 40:
+            drone.move_up(0.05)
+        elif (bcent(2,2)-fcent(2,2)) < -40:
+            drone.move_down(0.05)
+        else:
+            drone.move_stop#ホバリングのまま　このプログラムは対話時も維持
+以下CV-FACEのmainよりコピペ"""
+
+            cx = int( x + w/2 )
+            cy = int( y + h/2 )
+
+            a = b = c = d = 0   # rcコマンドの初期値は0
+
+            # 目標位置との差分にゲインを掛ける（P制御)
+            dx = 0.4 * (240 - cx)       # 画面中心との差分
+            dy = 0.4 * (180 - cy)       # 画面中心との差分
+            dw = 0.8 * (100 - w)        # 基準顔サイズ100pxとの差分
+
+            dx = -dx # 制御方向が逆だったので，-1を掛けて逆転させた
+
+            print('dx=%f  dy=%f  dw=%f'%(dx, dy, dw) )  # printして制御量を確認できるように
+
+            # 旋回方向の不感帯を設定
+            d = 0.0 if abs(dx) < 20.0 else dx   # ±20未満ならゼロにする
+            # 旋回方向のソフトウェアリミッタ(±100を超えないように)
+            d =  100 if d >  100.0 else d
+            d = -100 if d < -100.0 else d
+
+            # 前後方向の不感帯を設定
+            b = 0.0 if abs(dw) < 10.0 else dw   # ±10未満ならゼロにする
+            # 前後方向のソフトウェアリミッタ
+            b =  100 if b >  100.0 else b
+            b = -100 if b < -100.0 else b
+
+
+            # 上下方向の不感帯を設定
+            c = 0.0 if abs(dy) < 30.0 else dy   # ±30未満ならゼロにする
+            # 上下方向のソフトウェアリミッタ
+            c =  100 if c >  100.0 else c
+            c = -100 if c < -100.0 else c
+
+            # rcコマンドを送信
+            #drone.send_command('rc %s %s %s %s'%(int(a), int(b), int(c), int(d)) )
+
 
         else:
-            drone.to_detect()
+            self.drone.approach_flag　== 1#見つけたから
+            self.drone.to_detect()
         #detect側に移動する
         # 追跡が失敗した場合にdrone.detect_flagを倒す
         # 追跡が成功し，接近できた場合にはdrone.close_flagを立てる
-        return tracker
