aec215688362f393af992d31e75b526dfeb5db45
Mine <mine.go.0525@gmail.com>
2020-01-20 16:51:41 +0900

    [fix]もう一回

--- SystemPJ/rec_human/approachModule.py
+++ SystemPJ/rec_human/approachModule.py
@@ -38,6 +38,22 @@ class Approach:
         self.tracker = self.select_tracker(track_type) # 指定されたタイプのトラッカーインスタンスの作成
         self.tracker.init(frame, bbox) # 作成したトラッカーの初期化．画像と認識した人の領域を与える
 
+    def select_tracker(self, track_type):
+        """
+        コンストラクタで用いるトラッカー初期化用のメソッド
+
+        Args:
+            track_type str:
+                指定されたタイプのトラッカー
+
+        Returns:
+            tracker obj:
+                指定されたタイプのトラッカーインスタンス
+        """
+        tracker = cv2.TrackerBoosting_create() # テスト用に入れた
+
+        return tracker
+
     def approach(self, frame):
         """
         認識した人をトラッカーを用いて追跡するメソッド
