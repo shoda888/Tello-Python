a3226e642df871348973341625a104c49c502214
kenkenhed <kenk1543@gmail.com>
2020-01-20 14:44:25 +0900

    new_file

---
+++ openpose/play.py
@@ -0,0 +1,19 @@
+import run_function
+import cv2
+
+path = './images/iwasaki7.jpg'
+input_img = cv2.imread(path, cv2.IMREAD_COLOR)
+
+run_function.openpose(image=input_img)
+ 
+
+# path = './images/p1.jpg'
+# input_img = cv2.imread(path, cv2.IMREAD_COLOR)
+
+# run_function.openpose(image=input_img)
+
+
+# path = './images/p1.jpg'
+# input_img = cv2.imread(path, cv2.IMREAD_COLOR)
+
+# run_function.openpose(image=input_img)
