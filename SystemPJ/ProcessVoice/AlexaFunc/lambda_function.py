103794f6520c03dea09267559567f7dc68fc6d07
shoda888 <gekka888@gmail.com>
2020-01-25 12:09:07 +0900

    Helloworld Intent 削除

--- SystemPJ/ProcessVoice/AlexaFunc/lambda_function.py
+++ SystemPJ/ProcessVoice/AlexaFunc/lambda_function.py
@@ -61,23 +61,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
                 .response
         )
 
-
-class HelloWorldIntentHandler(AbstractRequestHandler):
-    """Handler for Hello World Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("HelloWorldIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "Hello World!"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
     
 class ReplyIntentHandler(AbstractRequestHandler):
     """Handler for Reply Intent."""
@@ -219,7 +202,6 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(ReplyIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
